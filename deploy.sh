1c751cd6f64965acea2cf977cc4910965a7085b3
SUMIN <suminme@users.noreply.github.com>
2016-04-05 16:38:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,8 @@ else
 export JAVA_OPTS=$2
 fi
 echo "JAVA_OPTS=\"$JAVA_OPTS\"" >> $CATALINA_HOME/bin/setenv.sh
-echo "Used JAVA_OPTS:"+$JAVA_OPTS
+echo "-Used JAVA_OPTS:"+$JAVA_OPTS+"-"
 
-# Start tomcat
-$CATALINA_HOME/startup.sh && tail -f $CATALINA_HOME/logs/catalina.out
+# Clean & Start tomcat
+rm -rf $CATALINA_HOME/temp/* && rm -rf $CATALINA_HOME/work/* && rm -rf $CATALINA_HOME/webapps/*
+$CATALINA_HOME/bin/startup.sh && tail -f $CATALINA_HOME/logs/catalina.out
