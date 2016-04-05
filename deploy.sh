630a4a87f33da59bcba0a5249b609ddcbf22034d
SUMIN <suminme@users.noreply.github.com>
2016-04-05 16:45:11 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,9 @@ if [ ! -n "$2" ];
 then
 export JAVA_OPTS="-Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -Xms256m -Xmx1024m"
 else
-export JAVA_OPTS=$2
+export JAVA_OPTS=\"$2\"
 fi
 echo "JAVA_OPTS=\"$JAVA_OPTS\"" >> $CATALINA_HOME/bin/setenv.sh
-echo "-JAVA_OPTS:"$JAVA_OPTS"-"
 
 # Clean & Start tomcat
 rm -rf $CATALINA_HOME/temp/* && rm -rf $CATALINA_HOME/work/* && rm -rf $CATALINA_HOME/webapps/*
