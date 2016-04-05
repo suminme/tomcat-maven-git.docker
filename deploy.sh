77827a0935c3732815205eb55da9c922fa59e366
SUMIN <suminme@users.noreply.github.com>
2016-04-05 17:06:03 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,23 @@ export JAVA_OPTS="$2"
 fi
 echo "JAVA_OPTS=\"$JAVA_OPTS\"" >> $CATALINA_HOME/bin/setenv.sh
 
+# Git clone or update
+export CODE_PATH /opt/code
+
+if [ ! -d $CODE_PATH ];
+then
+git clone $1 $CODE_PATH
+else
+cd $CODE_PATH && git pull origin master
+fi
+
+# Compile code
+cd $CODE_PATH && mvn clean package -Dmaven.test.skip=true
+
+# Deploy code
+cp $CODE_PATH/target/*.war $CATALINA_HOME/webapps
+mv $CATALINA_HOME/webapps/*.war $CATALINA_HOME/webapps/ROOT.war
+
 # Clean & Start tomcat
 rm -rf $CATALINA_HOME/temp/* && rm -rf $CATALINA_HOME/work/* && rm -rf $CATALINA_HOME/webapps/*
 $CATALINA_HOME/bin/startup.sh && tail -f $CATALINA_HOME/logs/catalina.out
