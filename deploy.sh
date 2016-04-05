4265be4a5e2054329b27a84a4beaabb6f817dc05
SUMIN <suminme@users.noreply.github.com>
2016-04-05 16:17:13 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
 
-# Check git repository
+# Check Git Repository
 if [ ! -n "$1" ];
 then
-echo "no git repository input"
+echo "no git repository"
 return;
 fi;
+
+# Check JAVA_OPTS
+if [ ! -n "$2" ];
+then
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -Duser.timezone=GMT+08 -Xms256m -Xmx1024m"
+else
+export JAVA_OPTS=$2
+fi
+echo "JAVA_OPTS=\"$JAVA_OPTS\"" >> $CATALINA_HOME/bin/setenv.sh
+
+# Start tomcat
+startup.sh && tail -f $CATALINA_HOME/logs/catalina.out
